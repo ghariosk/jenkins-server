a280d632c5d86cff0f36887ada426430111fc8b4
ghariosk <karlgharios@hotmail.com>
2017-10-28 23:14:04 +0100

    wrote the unit tests for the jenkins cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,49 +4,6 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-# package 'python3-software-properties' do 
-# 	action :install
-# end
-
-# package 'software-properties-common' do 
-# 	action :install
-# end
-
-#adds the java ppa repository to the server
-# apt_repository 'openjdk' do
-# 	action :add
-# 	uri 'ppa:openjdk-r/ppa'
-# 	distribution 'trusty'
-# end
-
-# apt_repository 'openjdk' do
-# 	action :add
-# 	uri 'ppa:webupd8team/java'
-# end
-
-
-# apt_update 'update' do
-# 	action :update
-# end
-
-# apt_package 'oracle-java8-installer' do
-# 	action :install
-# end
-
-
-
-
-# apt_package 'openjdk-8-jre' do
-# 	action :install
-# end
-
-# apt_package 'openjdk-8-jdk' do
-# 	action :install
-# end
-
-
-
-
 apt_repository "webupd8team" do
   uri "http://ppa.launchpad.net/webupd8team/java/ubuntu"
   components ['main']
@@ -54,14 +11,11 @@ apt_repository "webupd8team" do
   keyserver "keyserver.ubuntu.com"
   key "EEA14886"
   deb_src true
-
 end
 
 apt_update 'update' do
 	action :update
 end
-
-
 
 execute "accept-license" do
   command "echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections"
@@ -71,20 +25,9 @@ package "oracle-java8-installer" do
   action :install
 end
 
-
-
-
 package "oracle-java8-set-default" do
   action :install
 end
-
-
-# apt_repository "jenkins" do
-#   uri "https://pkg.jenkins.io/debian-stable binary/"
-#   key "https://pkg.jenkins.io/debian-stable/jenkins.io.key"
- 
-#   action :add
-# end
 
 execute "add key" do 
 	command "wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -"
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -18,5 +18,33 @@ describe 'jenkins-server::default' do
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
+
+    it 'should add the repository for Java' do 
+      expect(chef_run).to add_apt_repository 'webupd8team'
+    end
+
+    it 'installs java-8 installer' do
+      expect(chef_run).to install_package "oracle-java8-installer"
+    end
+
+   it 'installs java-8 ' do
+      expect(chef_run).to install_package "oracle-java8-set-default"
+    end
+
+    it 'update' do
+      expect(chef_run).to update_apt_update 'update'
+    end
+
+    it 'should replace the source.list' do
+      expect(chef_run).to create_template '/etc/apt/sources.list'
+    end
+
+    it 'should install jenkins' do
+      expect(chef_run).to install_package 'jenkins'
+    end
+
+    it 'should enable jenkins' do
+      expect(chef_run).to enable_service 'jenkins'
+    end
   end
 end
